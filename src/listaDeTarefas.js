e146c515af572f6194d3512ce5acc79fb011f30d
CamilaVerso <camilaverso2@gmail.com>
2023-09-09 22:18:31 -0300

    Inclui a função de sinalizar como concluída a tarefa

--- src/listaDeTarefas.js
+++ src/listaDeTarefas.js
@@ -3,7 +3,7 @@ const criarTarefa = (e) => {
 	const lista = document.querySelector('[data-list]');
 	const input = document.querySelector('[data-form-input]');
 	const valor = input.value;
-	
+
 	const tarefa = document.createElement('li');
 	tarefa.classList.add('task');
 
@@ -11,10 +11,32 @@ const criarTarefa = (e) => {
 
 	tarefa.innerHTML = conteudo;
 
-	lista.appendChild(tarefa);	
+	tarefa.appendChild(BotaoConclui());
+	lista.appendChild(tarefa);
 	input.value = " ";
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
 novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConclui = () => {
+	const botaoConclui = document.createElement('button');
+
+	botaoConclui.classList.add('check-button');
+	botaoConclui.innerText = 'concluir'
+
+	botaoConclui.addEventListener('click', concluirTarefa);
+
+	return botaoConclui;
+
+}
+
+const concluirTarefa = (e) => {
+	const botaoConclui = e.target;
+
+	const tarefaCompleta = botaoConclui.parentElement;
+
+	tarefaCompleta.classList.toggle('done');
+
+}
